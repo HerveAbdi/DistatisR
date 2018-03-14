ad33b8cc98996b3e37406ec6f68944be71eb396d
Herve Abdi <herve@utdallas.edu>
2018-03-14 17:26:09 -0500

    Add DistanceFromRank

---
+++ R/DistanceFromRank.R
@@ -0,0 +1,81 @@
+#---------------------------------------------------------------------
+# A function to create a distance matrix between elements
+# that have been ranked.
+# The distance is the plain Euclidean/Spearman rank
+# HA October 2015
+# Input is a rectangular I stimuli by J participants  matrix
+# gives back a I*I*J "cube of distance: to be used by
+# DistatisR
+# Current Version is March 14, 2018.
+#---------------------------------------------------------------------
+#' \code{DistanceFromRank}:
+#' Creates a 3-dimensional distance array from the results 
+#' of a ranking task.
+#' 
+#' \code{DistanceFromRank}:  
+#' Takes the results from a (plain) ranking task 
+#' where \eqn{K} assessors rank (with possible ties)
+#' \eqn{I} observations on 
+#' one dimension and transform it into a brick of data
+#' to be used by \code{distatis}.
+#' 
+#' @details    
+#'  \code{DistanceFromRank} creates an \eqn{I \times
+#' I \times K}{I*I*K} array of distance 
+#' in which each of the \eqn{K} "slices"
+#' stores the (squared Euclidean ranking) 
+#' distance matrix of the \eqn{k}th assessor.  
+#' In one of
+#' these distance matrices the distance 
+#' between two objects is computed
+#' from he Pythogorean theorem. 
+#' The ouput ot the function \code{DistanceFromRank} 
+#' is used as input for the
+#' function \code{\link{distatis}}.
+#'
+#' The input should have assessors as columns and observations 
+#' as rows (see
+#' example below)
+#' 
+#' @param X gives the results of a ranking task 
+#' (see example below) as an
+#' objects (rows) by assessors (columns) matrix.
+#' @return \code{DistanceFromRank} returns an
+#'  \eqn{I\times I \times K}{I*I*K}
+#' array of distance.
+#' @author Herve Abdi
+#' @seealso \code{\link{distatis}} \code{\link{DistanceFromSort}} 
+#' @export
+#' @examples
+#' # Use the data set WinesRankingRawData stored in an excel file.
+#' path2file <- system.file("extdata",
+#'            "WinesRankingRawData.xlsx", package = 'DistatisR')
+#' ranking6Wines <- read.df.excel(path = path2file, sheet = 'Ranking')
+#' aCubeOfDistance <- DistanceFromRank(ranking6Wines$df.data)
+#' 
+DistanceFromRank <- function(X){
+	# Private functions first
+	# Rank2Dist: 
+  # Create a sorting distance matrix from the ranking vector
+    Rank2Dist <- function(LeRank){# Start Rank2Dist
+	   nObj = length(LeRank)
+     truc = matrix(LeRank,nObj,nObj)
+     # D = (v - v')^2
+     # DistMat = abs(truc - t(truc)) # Euclidean
+     DistMat =  (truc - t(truc))^2 # squared Euclidean
+	 return(DistMat)
+     }# End Rank2Dist
+	# X is a matrix of sort 
+	# Each column is a participant
+  # Objects are rank-ordered or rated
+	# send back a Object*Object*Participant
+	# distance array
+	nI = nrow(X);nJ = ncol(X)
+	# initialize
+	LeCube2Distance = array(0, c(nI,nI,nJ))
+	# An Ugly loop
+	for(j in 1:nJ){LeCube2Distance[,,j]<-Rank2Dist(X[,j]) }
+	dimnames(LeCube2Distance) <-list(rownames(X),rownames(X),colnames(X))
+	return(LeCube2Distance)  
+} # End if function DistanceFromRank
+#---------------------------------------------------------------------
--- R/WinesRankingRawData.R
+++ R/WinesRankingRawData.R
@@ -4,7 +4,7 @@
 # Describes the Ranking 6 Wines Data
 # Created on March 12 2018 by Herve Abdi
 # as part of the DistatisR package.
-#
+# Remember to use roxygen2::roxygenize()
 
 #' \code{WinesRankingRawData}: an example of an excel file
 #' with (simulated) ranking data. Can be read with the function
@@ -16,7 +16,7 @@
 #' \code{read.df.excel}.
 #'
 #' @details
-#' In this example of a "ranking task" 
+#' In this example of a "ranking task," 
 #' 80 (simulated or fictitious) 
 #' assessors ranked 6 red wines from Burgundy (France). 
 #' The assessor first chooses
@@ -27,7 +27,14 @@
 #' The assessors were 40 men and 40 women (first character w/n)
 #' and 40 American or 40 French (second character a/f).
 #' The red wines that were tasted are Irancy, Saint-Brie,
-#' Beaune, Nuits (Cote de Nuits), Beaujolias, and Beaujolais-Nouveau.
+#' Beaune, Nuits (Cote de Nuits), Beaujolais, and Beaujolais-Nouveau.
+#' Irancy & Saint-Brie are near the cities of Auxerre and Chablis,
+#' Beaune & Cote de Nuits are from central Burgundy, and
+#' Beaujolais and Beaujolais Nouveau are from the south of Burgundy;
+#' Beaujolais Nouveau is a young wine (a primeur) 
+#' released in November of its year, 
+#' after only a few weeks of fermentation.
+#' 
 #' 
 #' @section Availability:
 #' To fetch this dataset use \code{system.file()}
